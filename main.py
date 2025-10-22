aac63d08d1c2944461b8a3f51169137586dd844a
Octavio Locatelli <octaviolocatelli@fucape.br>
2025-10-22 11:38:29 -0300

    código inicial

---
+++ main.py
@@ -0,0 +1,33 @@
+def soma(a,b):
+    #guilherme
+    pass
+
+def subtracao(a,b):
+    #duda
+    pass
+
+def multiplicacao(a,b):
+    #caua
+    pass
+
+def divisao(a,b):
+    #melissa
+    pass
+
+def exponencial(a,b):
+    #arthur
+    pass
+
+def raiz(a,b):
+    #gabriel
+    pass
+
+def log(a,b):
+    #mateus
+    pass
+
+def fatorial(a):
+    #lucas
+    pass
+
+# Menu: Carlos
